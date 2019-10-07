1652e2a1a8606f91708e6d4002ad9c43918c9dea
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-07 12:35:43 +0100

    refactor: use eval function on interpolated string

--- javascript/src/basic-maths-operations.js
+++ javascript/src/basic-maths-operations.js
@@ -1,17 +1,5 @@
 "use strict";
 
 module.exports = function basicOp(operation, value1, value2) {
-  switch (operation) {
-    case '+':
-      return value1 + value2
-      break;
-    case '-':
-      return value1 - value2
-      break;
-    case '*':
-      return value1 * value2
-      break;
-    case '/':
-      return value1 / value2
-  }
+  return eval(`${value1} ${operation} ${value2}`)
 }
