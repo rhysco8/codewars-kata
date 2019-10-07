b9884aaf1572b2d7a6eea1b577f7673d64ea13fe
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-07 12:32:12 +0100

    refactor: use switch statement

--- javascript/src/basic-maths-operations.js
+++ javascript/src/basic-maths-operations.js
@@ -1,13 +1,17 @@
 "use strict";
 
 module.exports = function basicOp(operation, value1, value2) {
-  if (operation === '+') {
-    return value1 + value2
-  } else if (operation === '-') {
-    return value1 - value2
-  } else if (operation === '*') {
-    return value1 * value2
-  } else if (operation === '/') {
-    return value1 / value2
+  switch (operation) {
+    case '+':
+      return value1 + value2
+      break;
+    case '-':
+      return value1 - value2
+      break;
+    case '*':
+      return value1 * value2
+      break;
+    case '/':
+      return value1 / value2
   }
 }
