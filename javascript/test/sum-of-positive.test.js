ea90fe6711f82755254e5769a240bf472c292ade
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-10 15:29:25 +0100

    test: returns 13 when passed [1, -2, 3, 4, 5]

--- javascript/test/sum-of-positive.test.js
+++ javascript/test/sum-of-positive.test.js
@@ -8,4 +8,8 @@ describe('sums only positive integers', () => {
   test('returns 15 when passed [1, 2, 3, 4, 5]', () => {
     expect(positiveSum([1, 2, 3, 4, 5])).toBe(15);
   });
+
+  test('returns 13 when passed [1, -2, 3, 4, 5]', () => {
+    expect(positiveSum([1, -2, 3, 4, 5])).toBe(13);
+  });
 });
