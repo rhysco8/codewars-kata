23b55c0423813c5d5aea3f10f389bf523e148151
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-10 14:34:21 +0100

    test: returns 3 when passed 2

--- javascript/src/grasshopper-summation.js
+++ javascript/src/grasshopper-summation.js
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports = function summation (number) {
-  return 1
+  var total = 0
+  for (var i = 0; i <= number; i++) {
+    total += i
+  }
+  return total
 }
--- javascript/test/grasshopper-summation.test.js
+++ javascript/test/grasshopper-summation.test.js
@@ -4,4 +4,8 @@ describe('summation adds numbers from 1 to n', () => {
   test('returns 1 when passed 1', () => {
     expect(summation(1)).toBe(1);
   });
+
+  test('returns 3 when passed 2', () => {
+    expect(summation(2)).toBe(3);
+  });
 });
