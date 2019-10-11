ba500493516f11e38c342af427aa04292388cfca
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-11 13:35:41 +0100

    test: returns [1, 8, 2] when passed ([1, 8, 2], [])

--- javascript/test/array-difference.test.js
+++ javascript/test/array-difference.test.js
@@ -6,4 +6,8 @@ describe('remove values from list a which are present in list b', () => {
   test('returns [] when passed ([], [4, 5])', () => {
     expect(arrayDiff([], [4, 5])).toStrictEqual([]);
   });
+
+  test('returns [1, 8, 2] when passed ([1, 8, 2], [])', () => {
+    expect(arrayDiff([1, 8, 2], [])).toStrictEqual([1, 8, 2]);
+  });
 });
