fb40907476143ceec5ae40fad9cf2cf99edaab1c
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-15 12:06:38 +0100

    refactor: rewrite nextNumber function to use reduce() on array slice

--- javascript/src/tribonacci.js
+++ javascript/src/tribonacci.js
@@ -2,16 +2,16 @@
 
 module.exports = function tribonacci(signature, n) {
   let sequence = (n <= signature.length) ? signature.slice(0, n) :
-    addNextInSequence(signature, n)
+    fillInSequence(signature, n)
   return sequence
 }
 
 function nextNumber(array) {
-  let lastElement = array.length - 1
-  return array[lastElement] + array[lastElement - 1] + array[lastElement - 2]
+  let lastThreeElements = array.slice(array.length - 3)
+  return lastThreeElements.reduce((a, b) => a + b, 0)
 }
 
-function addNextInSequence(array, n) {
+function fillInSequence(array, n) {
   let sequence = array
   while (sequence.length < n) {
     sequence.push(nextNumber(sequence))
