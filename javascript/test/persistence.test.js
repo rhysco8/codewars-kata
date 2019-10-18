884048f0d59d9bb3f7888b3b311eb77f34b7e005
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-10-18 12:17:01 +0100

    test: returns 4 given 999

--- javascript/test/persistence.test.js
+++ javascript/test/persistence.test.js
@@ -14,4 +14,8 @@ describe('returns mulitplicative persistence', () => {
   test('returns 2 given 25', () => {
     expect(persistence(25)).toBe(2);
   });
+
+  test('returns 4 given 999', () => {
+    expect(persistence(999)).toBe(4);
+  });
 });
