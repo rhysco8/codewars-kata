9bf987486fe87e613424b09fc22380dc83788503
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-11-26 14:24:40 +0000

    test: removes dash delimiters

--- javascript/src/convert-to-camel-case.js
+++ javascript/src/convert-to-camel-case.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = function toCamelCase(string) {
-  return string
+  return string.replace("-", "")
 }
--- javascript/test/convert-to-camel-case.test.js
+++ javascript/test/convert-to-camel-case.test.js
@@ -4,10 +4,14 @@ const toCamelCase = require('../src/convert-to-camel-case');
 
 describe('converts string to camel case', () => {
   test('returns empty string', () => {
-    expect(toCamelCase("")).toBe("")
+    expect(toCamelCase("")).toBe("");
   });
 
   test('returns single word', () => {
-    expect(toCamelCase("a")).toBe("a")
-  })
+    expect(toCamelCase("a")).toBe("a");
+  });
+
+  test('removes dash delimiters', () => {
+    expect(toCamelCase("a-B")).toBe("aB");
+  });
 });
